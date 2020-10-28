67ce0ac281de5f82a90d1d03183946a70452dde4
Gio Lodi <giovanni.lodi42@gmail.com>
2020-10-28 13:05:19 +1100

    Add logic to fetch PR state

--- bintray-garbage-collector.rb
+++ bintray-garbage-collector.rb
@@ -13,16 +13,37 @@ def get_bintray_versions(project)
   json['versions']
 end
 
-def extract_development_version(versions)
+def extract_development_versions(versions)
   versions
     .select { |v| v =~ /\d+-.*/ }
     .group_by { |v| v.split('-').first }
 end
 
+def delete_bintray_version(project, version)
+  puts "TODO: Make API call to delete #{version} for #{project}"
+end
+
+def get_pr_state(repo, id)
+  uri = URI("https://api.github.com/repos/#{repo}/pulls/#{id}")
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  request = Net::HTTP::Get.new(uri.request_uri)
+
+  response = http.request(request)
+  json = JSON.parse(response.body)
+
+  json['state']
+end
+
 projects = [
-  'mokagio/maven/utils-experiment'
+  { bintray: 'mokagio/maven/utils-experiment', github: 'mokagio/WordPress-Utils-Android' }
 ]
 
 projects.each do |project|
-  puts extract_development_version(get_bintray_versions(project))
+  extract_development_versions(get_bintray_versions(project[:bintray])).each do |pr_id, binaries|
+    next unless get_pr_state(project[:github], pr_id) == 'closed'
+    binaries.each do |version|
+      delete_bintray_version(project[:bintray], version)
+    end
+  end
 end
