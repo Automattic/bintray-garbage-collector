7a797121941f905591370275b7aa1806c878e317
Gio Lodi <giovanni.lodi42@gmail.com>
2020-11-06 14:37:22 +1100

    Add "..." suffix and ">" prefix to make log output clearer
    Rationale at https://github.com/Automattic/bintray-garbage-collector/pull/6#discussion_r515287061
    
    Thanks to @AliSoftware for the suggestion!

--- lib/garbage_collector.rb
+++ lib/garbage_collector.rb
@@ -15,37 +15,37 @@ class GarbageCollector
 
   def run(projects)
     projects.each do |project|
-      log "Looking for versions to delete for #{project[:bintray]}"
+      log "Looking for versions to delete for #{project[:bintray]}..."
 
       dev_versions = @bintray.get_bintray_versions(project[:bintray])
 
       if dev_versions.empty?
-        log "No versions found. Moving on."
+        log "> No versions found. Moving on."
         next
       end
 
-      log "Found versions for PR(s): #{dev_versions.keys.join(', ')}"
+      log "> Found versions for PR(s): #{dev_versions.keys.join(', ')}."
 
       dev_versions.each do |pr_id, versions|
-        log "Checking if versions for PR #{pr_id} should be deleted"
+        log "Checking if versions for PR #{pr_id} should be deleted..."
 
         if @github.is_pr_closed?(project[:github], pr_id)
-          log "Deleting versions for PR #{pr_id}"
+          log "Deleting versions for PR #{pr_id}..."
         else
-          log "PR #{pr_id} is open. Skipping."
+          log "> PR #{pr_id} is open. Skipping."
           next
         end
 
         versions.each do |version|
           if @dry_run
-            log "DRY RUN! Deleted version #{version}"
+            log "> DRY RUN! Deleted version #{version}"
             next
           end
 
           if @bintray.delete_bintray_version(project[:bintray], version)
-            log "Deleted version #{version}"
+            log "> Deleted version #{version}."
           else
-            log "Failed to delete version #{version}"
+            log "> Failed to delete version #{version}."
           end
         end
       end
