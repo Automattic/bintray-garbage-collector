628145d65505b6bf6fa78fb48d741156cecdcad3
Gio Lodi <giovanni.lodi42@gmail.com>
2020-11-09 14:44:26 +1100

    Extract req generation and JSON response parsing in JSONHTTPClient
    Thanks for the suggestion @AliSoftware.
    
    See https://github.com/Automattic/bintray-garbage-collector/pull/6#discussion_r518636934

--- lib/json_http_client.rb
+++ lib/json_http_client.rb
@@ -5,22 +5,20 @@ require 'json'
 class JSONHTTPClient
 
   def get(uri:)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    request = Net::HTTP::Get.new(uri.request_uri)
-
-    response = http.request(request)
-
-    return nil unless response.kind_of? Net::HTTPSuccess
-
-    JSON.parse(response.body)
+    execute(request_class: Net::HTTP::Get, uri: uri)
   end
 
   def delete(uri:, auth:)
+    execute(request_class: Net::HTTP::Delete, uri: uri, auth: auth)
+  end
+
+  private
+
+  def execute(request_class:, uri:, auth: nil)
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
-    request = Net::HTTP::Delete.new(uri.request_uri)
-    request.basic_auth(auth.user, auth.password)
+    request = request_class.new(uri.request_uri)
+    request.basic_auth(auth.user, auth.password) unless auth.nil?
 
     response = http.request(request)
 
