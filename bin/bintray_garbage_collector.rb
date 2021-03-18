1df1c67931257bc77bd887bbe09cf87101d65bf7
Gio Lodi <giovanni.lodi42@gmail.com>
2021-03-18 15:21:54 +1100

    Add OptionParser to pass --verbose and --dry-run

--- bin/bintray_garbage_collector.rb
+++ bin/bintray_garbage_collector.rb
@@ -1,4 +1,5 @@
 require 'dotenv'
+require 'optparse'
 require 'psych'
 require_relative '../lib/garbage_collector.rb'
 
@@ -6,6 +7,24 @@ source = Psych.load(File.read('projects.yml'), symbolize_names: true)
 projects = source[:projects]
 
 Dotenv.load
+
+# Default options
+options = {
+  verbose: true,
+  dry_run: false
+}
+
+OptionParser.new do |parser|
+  parser.banner = "Usage: #{File.basename(__FILE__)} [options]"
+
+  parser.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+    options[:verbose] = v
+  end
+
+  parser.on('-d', '--dry-run', "Dry run") do
+    options[:dry_run] = true
+  end
+end.parse!
 
 def read_from_environment!(key)
   value = ENV[key]
@@ -17,6 +36,6 @@ end
 GarbageCollector.new(
   bintray_user: read_from_environment!('BINTRAY_USER'),
   bintray_key: read_from_environment!('BINTRAY_KEY'),
-  verbose: true,
-  dry_run: false
+  verbose: options[:verbose],
+  dry_run: options[:dry_run]
 ).run(projects)
